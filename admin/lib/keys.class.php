1c96ed0e171f9e64a6c10ec31d36a0658aa85600
Kevin Cala Sánchez <kevin@kcsystem.es>
2017-01-26 14:36:19 +0100

    #2 - Done
    Ahora funciona els sistema de control de la KEY.
    Aún no está implementado en el código cliente

--- admin/lib/keys.class.php
+++ admin/lib/keys.class.php
@@ -1,98 +1,89 @@
 <?php
-
-/* 
- * The MIT License
- *
- * Copyright 2016 Kyto.
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
- */
-/**
- * 
- * @row KeyType 1 = Private, 2 = Public
- * @row KeyMode 1 = Test, 2 = Live
- */
-class keys {
-    
-    
-    /**
-     * Return all the active keys in an array with all the key_data table info
-     * @return array
+    /*
+     * The MIT License
+     *
+     * Copyright 2016 Kyto.
+     *
+     * Permission is hereby granted, free of charge, to any person obtaining a copy
+     * of this software and associated documentation files (the "Software"), to deal
+     * in the Software without restriction, including without limitation the rights
+     * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+     * copies of the Software, and to permit persons to whom the Software is
+     * furnished to do so, subject to the following conditions:
+     *
+     * The above copyright notice and this permission notice shall be included in
+     * all copies or substantial portions of the Software.
+     *
+     * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+     * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+     * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+     * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+     * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+     * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+     * THE SOFTWARE.
      */
-    public static function getKeys()
+
+    /**
+     * 
+     * @row KeyType 1 = Private, 2 = Public
+     * @row KeyMode 1 = Test, 2 = Live
+     */
+    class keys
     {
-        try {
-            $query = 'SELECT * FROM '.DB_DATABASE.'.key_data k';
-            Foreach ($dbp->query($query) as $row){
-                $result[] = $row;
+
+        /**
+         * Return all the active keys in an array with all the key_data table info
+         * @return array
+         */
+        public static function getKeys()
+        {
+            try {
+                $query = 'SELECT * FROM '.DB_DATABASE.'.key_data k';
+                Foreach ($dbp->query($query) as $row) {
+                    $result[] = $row;
+                }
+                return true;
+            } catch (Exception $ex) {
+                return false;
             }
-            return true;
-        } catch (Exception $ex) {
-            return false;
         }
-    }
-    
-    public static function setEnabledKeys($id, $active = 0)
-    {
-        try{
-            $query = 'UPDATE '.DB_DATABASE.'.key_data k SET k.status = :status WHERE k.id = :id ;';
-            $dbp->prepare($query)->execute(array(':status' => $active, ':id' => $id));
-            return true;
-        } catch (Exception $ex) {
-            return false;
+
+        public static function setEnabledKeys($dbp, $id, $active = 0)
+        {
+            try {
+                $query = 'UPDATE '.DB_DATABASE.'.key_data k SET k.status = :status WHERE k.id = :id ;';
+                $dbp->prepare($query)->execute(array(':status' => $active, ':id' => $id));
+                return true;
+            } catch (Exception $ex) {
+                return false;
+            }
         }
-    }
 
-    public static function setDisableKey($id)
-    {
-        self::setEnabledKeys($id, 0);
-    }
-
-    public static function setEnableKey($id)
-    {
-        self::setEnabledKeys($id, 1);
-    }
-
-    public static function createKey($dbp, $key_name, $key_type, $key_mode, $key_data, $key_status)
-    {
-        try {
-            $query = 'INSERT INTO '.DB_DATABASE.'.key_data (key_name, key_type, key_mode, key_data, status) VALUES(:name, :type, :mode, :data, :status)';
-            $dbp->prepare($query)->execute(array(
-                ':name' => $key_name,
-                ':type' => $key_type,
-                ':mode' => $key_mode,
-                ':data' => $key_data,
-                ':status' => $key_status));
-            return true;
-        } catch (Exception $ex) {
-            return $e->getMessage();
+        public static function createKey($dbp, $key_name, $key_type, $key_mode,
+                                         $key_data, $key_status)
+        {
+            try {
+                $query = 'INSERT INTO '.DB_DATABASE.'.key_data (key_name, key_type, key_mode, key_data, status) VALUES(:name, :type, :mode, :data, :status)';
+                $dbp->prepare($query)->execute(array(
+                    ':name' => $key_name,
+                    ':type' => $key_type,
+                    ':mode' => $key_mode,
+                    ':data' => $key_data,
+                    ':status' => $key_status));
+                return true;
+            } catch (Exception $ex) {
+                return $e->getMessage();
+            }
         }
-    }
 
-    public static function deleteKey($dbp, $key_id)
-    {
-        try {
-            $query = 'DELETE FROM '.DB_DATABASE.'.key_data WHERE id = :id';
-            $dbp->prepare($query)->execute(array(':id' => $key_id));
-            return true;
-        } catch (Exception $ex) {
-            return $e->getMessage();
+        public static function deleteKey($dbp, $key_id)
+        {
+            try {
+                $query = 'DELETE FROM '.DB_DATABASE.'.key_data WHERE id = :id';
+                $dbp->prepare($query)->execute(array(':id' => $key_id));
+                return true;
+            } catch (Exception $ex) {
+                return $e->getMessage();
+            }
         }
-    }
-}
+    }    
--- admin/pages/options/keys.page.php
+++ admin/pages/options/keys.page.php
@@ -1,102 +1,102 @@
 <?php
-/* 
- * The MIT License
- *
- * Copyright 2016 kyto.
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
- */
+    /*
+     * The MIT License
+     *
+     * Copyright 2016 kyto.
+     *
+     * Permission is hereby granted, free of charge, to any person obtaining a copy
+     * of this software and associated documentation files (the "Software"), to deal
+     * in the Software without restriction, including without limitation the rights
+     * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+     * copies of the Software, and to permit persons to whom the Software is
+     * furnished to do so, subject to the following conditions:
+     *
+     * The above copyright notice and this permission notice shall be included in
+     * all copies or substantial portions of the Software.
+     *
+     * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+     * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+     * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+     * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+     * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+     * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+     * THE SOFTWARE.
+     */
 ?>
 
 <script>
     var lool;
-    $(function(){
-       $("#createKey").click(function(){
-           $.ajax({
-               url: "<?php echo API_DIR; ?>",
-               method: "POST",
-               data: {
-                 action:'createKey',
-                 key_name: $("#key_name").val(),
-                 key_type: $("#key_type").val(),
-                 key_mode: $("#key_mode").val(),
-                 key_data: $("#key_data").val(),
-                 key_status: $("#key_status").val()
-                }
-           }).done(function(data){
-               data = JSON.parse(data);
-               console.log(data);
-               if (data['error'] == false){
-                   location.reload();
-                }else{
-                    $("#panel-text").html(data['message']);
-                    $("#error_panel").show();
-                }
-           }).fail(function(){
-               $("#panel-text").html("La Cagaste!!");
-               $("#error_panel").show();
-           });
-       });
-       $(".deleteKey").click(function(){
+    $(function () {
+        $("#createKey").click(function () {
             $.ajax({
                 url: "<?php echo API_DIR; ?>",
                 method: "POST",
                 data: {
-                 action: 'deleteKey',
-                 key_id: $(this).data("id")
+                    action: 'createKey',
+                    key_name: $("#key_name").val(),
+                    key_type: $("#key_type").val(),
+                    key_mode: $("#key_mode").val(),
+                    key_data: $("#key_data").val(),
+                    key_status: $("#key_status").val()
                 }
-            }).done(function(data){
-               data = JSON.parse(data);
-               console.log(data);
-               if (data['error'] == false){
-                   location.reload();
-                }else{
+            }).done(function (data) {
+                data = JSON.parse(data);
+                console.log(data);
+                if (data['error'] == false) {
+                    location.reload();
+                } else {
                     $("#panel-text").html(data['message']);
                     $("#error_panel").show();
                 }
-           }).fail(function(){
-               $("#panel-text").html("La Cagaste!!");
-               $("#error_panel").show();
-           });
+            }).fail(function () {
+                $("#panel-text").html("La Cagaste!!");
+                $("#error_panel").show();
+            });
+        });
+        $(".deleteKey").click(function () {
+            $.ajax({
+                url: "<?php echo API_DIR; ?>",
+                method: "POST",
+                data: {
+                    action: 'deleteKey',
+                    key_id: $(this).data("id")
+                }
+            }).done(function (data) {
+                data = JSON.parse(data);
+                console.log(data);
+                if (data['error'] == false) {
+                    location.reload();
+                } else {
+                    $("#panel-text").html(data['message']);
+                    $("#error_panel").show();
+                }
+            }).fail(function () {
+                $("#panel-text").html("La Cagaste!!");
+                $("#error_panel").show();
+            });
         });
 
-        $(".changeStatus").click(function(){
+        $(".changeStatus").click(function () {
             $.ajax({
                 url: "<?php echo API_DIR; ?>",
                 method: "POST",
                 data: {
-                 action: 'changeKeyStatus',
-                 key_id: $(this).data("id"),
-                 key_status: $(this).data("status")
+                    action: 'changeKeyStatus',
+                    key_id: $(this).data("id"),
+                    key_status: $(this).data("status")
                 }
-            }).done(function(data){
-               data = JSON.parse(data);
-               if (data['error'] == false){
-                   location.reload();
-                }else{
+            }).done(function (data) {
+                data = JSON.parse(data);
+                if (data['error'] == false) {
+                    location.reload();
+                } else {
                     $("#panel-text").html(data['message']);
                     $("#error_panel").show();
                 }
-           }).fail(function(){
-               $("#panel-text").html("La Cagaste!!");
-               $("#error_panel").show();
-           });
+            }).fail(function () {
+                $("#panel-text").html("La Cagaste!!");
+                $("#error_panel").show();
+            });
         });
     });
 </script>
@@ -128,18 +128,26 @@
                             </thead>
                             <tbody>
                                 <?php
-                                $query = 'SELECT * FROM '.DB_DATABASE.'.key_data k';
-                                Foreach ($dbp->query($query) as $row) {
-                                    ?>
-                                <tr class="<?php echo (($row['status']) ? 'key_enabled' : 'key_disabled') ?>">
-                                        <td><?php echo $row['key_name'] ?></td>
-                                        <td><?php echo ($row['key_type'] == 1 ? 'Private' : 'Public') ?></td>
-                                        <td><?php echo ($row['key_mode'] == 1 ? 'Test' : 'Live') ?></td>
+                                    $query = 'SELECT * FROM '.DB_DATABASE.'.key_data k';
+                                    Foreach ($dbp->query($query) as $row) {
+                                        ?>
+                                        <tr class="<?php
+                                        echo (($row['status']) ? 'key_enabled' : 'key_disabled')
+                                        ?>">
+                                            <td><?php echo $row['key_name'] ?></td>
+                                            <td><?php
+                                                echo ($row['key_type'] == 1 ? 'Private'
+                                                        : 'Public')
+                                                ?></td>
+                                            <td><?php
+                                                echo ($row['key_mode'] == 1 ? 'Test'
+                                                        : 'Live')
+                                                ?></td>
                                             <td><?php echo $row['key_data'] ?></td>
                                             <td><i data-id="<?php echo $row['id']; ?>" class="deleteKey glyphicon glyphicon-trash"></i>&nbsp;<i data-id="<?php echo $row['id']; ?>" data-status="<?php echo $row['status'] ?>" class="changeStatus glyphicon glyphicon-off"></i>
-                                                </td>
+                                            </td>
                                         </tr>
-                                    <?php } ?>
+    <?php } ?>
                             </tbody>
                         </table>
                     </div>
@@ -171,7 +179,7 @@
                                 <tr>
                                     <td><input type="text" id="key_name"></td>
                                     <td><select id="key_type"><option value="1">Private</option><option value="2">Public</option></select></td>
-                                    <td><select id="key_mode"><option value="1">Private</option><option value="2">Public</option></select></td>
+                                    <td><select id="key_mode"><option value="1">Development (TEST)</option><option value="2">Production (LIVE)</option></select></td>
                                     <td><input type="text" id="key_data"></td>
                                     <td><select id="key_status"><option value="1">Enabled</option><option value="0">Disabled</option></select></td>
                                     <td><button id="createKey" class="btn btn-success btn-block">Create Key</button></td>
